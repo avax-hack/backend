df4df5b0b5190990f24a33b478f567776b3077ad
kream-cat <mycatiskream@gmail.com>
2026-03-10 07:29:44 +0900

    chore(observer): change default poll interval back to 2s

--- crates/observer/src/config_local.rs
+++ crates/observer/src/config_local.rs
@@ -2,9 +2,9 @@ use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref POLL_INTERVAL_MS: u64 = std::env::var("OBSERVER_POLL_INTERVAL_MS")
-        .unwrap_or_else(|_| "1000".to_string())
+        .unwrap_or_else(|_| "2000".to_string())
         .parse()
-        .unwrap_or(1000);
+        .unwrap_or(2000);
 
     pub static ref BATCH_SIZE: u64 = std::env::var("OBSERVER_BATCH_SIZE")
         .unwrap_or_else(|_| "100".to_string())
